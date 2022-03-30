82c13c68183a390ebf1b6e2487fd5f544ded2080
Aikanysh Begilerova <abegilerova@gmail.com>
2022-03-30 13:11:50 -0500

    Phonebook backend step6 is done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let persons = [
     { 
@@ -61,11 +62,43 @@ app.get('/api/persons/:id', (request, response)=>{
 
 app.delete('/api/persons/:id', (request, response)=>{
   const id = Number(request.params.id)
- 
   const person = persons.find(person => person.id === id)
-  response.status(204).end()
- 
+  response.status(204).end() 
+})
+
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map(p=>p.id) ) : 0 
+  return maxId + 1
+}
+
+app.post('/api/persons/', (request, response) => {  
+  const body = request.body
+
+  let nameExists = persons.some(person => person.name === body.name)
   
+  if (!body.name ){
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  } else if (!body.number){
+    return response.status(400).json({
+      error: 'number is missing'
+    }) 
+  } else if (nameExists){
+    return response.status(400).json({
+      error: 'name already exists'
+    })
+
+    }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number : body.number
+  }
+
+  persons= persons.concat(person)
+  response.json(person)
 })
 
 const PORT = process.env.PORT || 3001
