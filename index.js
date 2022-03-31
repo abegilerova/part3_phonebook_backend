9bcaf988b9b5c1ec67e8f7b137935268c6d96ad6
Aikanysh Begilerova <abegilerova@gmail.com>
2022-03-31 15:11:12 -0500

    Phonebook database, step1 is done

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
 app.use(express.json())
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
+
+
 
 app.use(cors())
 app.use(express.static('build'))
 
+// const url =
+// `mongodb+srv://fullstackmongo:Beknazar1@cluster0.jpc0j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+// mongoose.connect(url)
 
 app.use(morgan((tokens, req, res) => {
   return [
@@ -19,36 +26,15 @@ app.use(morgan((tokens, req, res) => {
   ].join(' ')
 }))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
 
 app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
     response.json(persons)
+  })
+    
   })
 
 
